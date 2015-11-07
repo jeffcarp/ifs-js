822610896f8479b87d6039814cff9679a95e0141
Jeff Carpenter <gcarpenterv@gmail.com>
2015-11-07 11:12:39 -0800

    Put everything in an IIFE

--- index.js
+++ index.js
@@ -1,117 +1,121 @@
-var canvas = document.getElementById('main-canvas');
-var context = canvas.getContext("2d");
+!(function () {
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  var canvas = document.getElementById('main-canvas');
+  var context = canvas.getContext("2d");
 
-var originX = window.innerWidth / 2;
-var originY = window.innerHeight / 2;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-scaleCanvas(canvas);
+  var originX = window.innerWidth / 2;
+  var originY = window.innerHeight / 2;
 
-var squareSize = 5e4;
+  scaleCanvas(canvas);
 
-var mag = 200;
-var xy = [0, 0];
-var iteration = 0;
-var step = 0.0006;
+  var squareSize = 5e4;
 
-context.fillStyle = "rgba(0, 0, 0, 0.008)";
+  var mag = 200;
+  var xy = [0, 0];
+  var iteration = 0;
+  var step = 0.0006;
 
-//var a = 0.97;
-var a = getRandomInt(-4, 5);
-//var b = -1.90;
-var b = getRandomInt(-4, 5);
-//var c = 1.38;
-var c = getRandomInt(-4, 5);
-//var d = -1.50;
-var d = getRandomInt(-4, 5);
+  context.fillStyle = "rgba(0, 0, 0, 0.008)";
 
-var up = false;
+  //var a = 0.97;
+  var a = getRandomInt(-4, 5);
+  //var b = -1.90;
+  var b = getRandomInt(-4, 5);
+  //var c = 1.38;
+  var c = getRandomInt(-4, 5);
+  //var d = -1.50;
+  var d = getRandomInt(-4, 5);
 
-var divA = document.getElementById('a');
-var divB = document.getElementById('b');
-var divC = document.getElementById('c');
-var divD = document.getElementById('d');
+  var up = false;
 
-function render() {
-  iteration += 1;
-  //context.clearRect(0, 0, canvas.width, canvas.height);
+  var divA = document.getElementById('a');
+  var divB = document.getElementById('b');
+  var divC = document.getElementById('c');
+  var divD = document.getElementById('d');
 
-  if (c >= 4) {
-    up = false;
-  } else if (c <= -4) {
-    up = true;
-  }
+  function render() {
+    iteration += 1;
+    //context.clearRect(0, 0, canvas.width, canvas.height);
 
-  up ? c += step
-     : c -= step;
+    if (c >= 4) {
+      up = false;
+    } else if (c <= -4) {
+      up = true;
+    }
 
-  up ? b += step 
-     : b -= step;
+    up ? c += step
+       : c -= step;
 
-  if (divA) {
-    divA.innerHTML = a;
-    divB.innerHTML = b;
-    divC.innerHTML = c;
-    divD.innerHTML = d;
-  }
+    up ? b += step 
+       : b -= step;
 
-  for (var i = 0; i < squareSize; i++) {
-    fillFromAbsoluteOrigin(xy[0], xy[1]);
-    xy = deJongIFS(xy[0], xy[1], a, b, c, d);
+    if (divA) {
+      divA.innerHTML = a;
+      divB.innerHTML = b;
+      divC.innerHTML = c;
+      divD.innerHTML = d;
+    }
+
+    for (var i = 0; i < squareSize; i++) {
+      fillFromAbsoluteOrigin(xy[0], xy[1]);
+      xy = deJongIFS(xy[0], xy[1], a, b, c, d);
+    }
+
+    window.requestAnimationFrame(render);
   }
 
   window.requestAnimationFrame(render);
-}
 
-window.requestAnimationFrame(render);
-
-function fillFromAbsoluteOrigin(x, y) {
-  x = (x * mag) + originX;
-  y = (y * mag) + originY;
-  context.fillRect(x, y, 1, 1);
-}
-
-// Returns a random integer between min (included) and max (excluded)
-// Using Math.round() will give you a non-uniform distribution!
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function randNth() {
-  var args = Array.prototype.slice.call(arguments);
-  return args[getRandomInt(0, args.length)];
-}
-
-function sIFS(x, y) {
-  var x2 = x / 2;
-  var y2 = y / 2;
-
-  return randNth(
-    [x2, y2],
-    [x2 + 0.5, y2 + 0.86],
-    [x2 + 1.0, y2]
-  );
-}
-
-function deJongIFS(x, y, a, b, c, d) {
-  return [
-    Math.sin(a * y) - Math.cos(b * x),
-    Math.sin(c * x) - Math.cos(d * y)
-  ];
-}
-
-// https://github.com/component/autoscale-canvas
-function scaleCanvas(canvas){
-  var ctx = canvas.getContext('2d');
-  var ratio = window.devicePixelRatio || 1;
-  if (1 != ratio) {
-    canvas.style.width = canvas.width + 'px';
-    canvas.style.height = canvas.height + 'px';
-    canvas.width *= ratio;
-    canvas.height *= ratio;
-    ctx.scale(ratio, ratio);
+  function fillFromAbsoluteOrigin(x, y) {
+    x = (x * mag) + originX;
+    y = (y * mag) + originY;
+    context.fillRect(x, y, 1, 1);
   }
-  return canvas;
-};
+
+  // Returns a random integer between min (included) and max (excluded)
+  // Using Math.round() will give you a non-uniform distribution!
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function randNth() {
+    var args = Array.prototype.slice.call(arguments);
+    return args[getRandomInt(0, args.length)];
+  }
+
+  function sIFS(x, y) {
+    var x2 = x / 2;
+    var y2 = y / 2;
+
+    return randNth(
+      [x2, y2],
+      [x2 + 0.5, y2 + 0.86],
+      [x2 + 1.0, y2]
+    );
+  }
+
+  function deJongIFS(x, y, a, b, c, d) {
+    return [
+      Math.sin(a * y) - Math.cos(b * x),
+      Math.sin(c * x) - Math.cos(d * y)
+    ];
+  }
+
+  // https://github.com/component/autoscale-canvas
+  function scaleCanvas(canvas){
+    var ctx = canvas.getContext('2d');
+    var ratio = window.devicePixelRatio || 1;
+    if (1 != ratio) {
+      canvas.style.width = canvas.width + 'px';
+      canvas.style.height = canvas.height + 'px';
+      canvas.width *= ratio;
+      canvas.height *= ratio;
+      ctx.scale(ratio, ratio);
+    }
+    return canvas;
+  };
+
+})();
